9cd26a6b3090304c9d4c16a6450ec39bdbcb1da1
Ramiro Martínez D'Elía <ramiro.m@geopagos.com>
2024-04-06 22:35:19 -0300

    Certificate & Private Key value holder.

---
+++ src/main/java/com/ramiromd/KeyAndCertHolder.java
@@ -0,0 +1,8 @@
+package com.ramiromd;
+
+public class KeyAndCertHolder {
+
+    final public static String CERT = "X509_CERTIFICATE_CONTENT";
+
+    final public static String KEY = "PRIVATE_KEY_CONTENT";
+}
