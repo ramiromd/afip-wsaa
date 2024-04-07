f781da4549627cc9024e05d0bfc5f0dd9fad63a4
Ramiro Martínez D'Elía <ramiro.m@geopagos.com>
2024-04-06 22:43:17 -0300

    Update gitignore & remove unnecesary class

--- src/main/java/com/ramiromd/KeyAndCertHolder.java
+++
@@ -1,8 +0,0 @@
-package com.ramiromd;
-
-public class KeyAndCertHolder {
-
-    final public static String CERT = "X509_CERTIFICATE_CONTENT";
-
-    final public static String KEY = "PRIVATE_KEY_CONTENT";
-}
